bc470d0bba823175d4b269b420ff56c1a67e25ac
CP <christopher.pyatt@postgrad.manchester.ac.uk>
2019-11-20 14:57:49 +0000

    add parser.py

---
+++ parser.py
@@ -0,0 +1,19 @@
+from sys import argv
+import argparse
+import glob
+import re
+import itertools
+from collections import defaultdict
+
+parser = argparse.ArgumentParser(description='Input a directory containing (in subdirectories for each strain) sequence files for cluster genes.')
+parser.add_argument('inputDir',
+                    help='Directory containing ONLY MapCoordinates.py output files (FASTA files of gene sequences). These can be in subdirectories if multiple strains.')
+parser.add_argument('outputFile',
+                    help='Desired name of output .txt file (saves to current working directory unless full path specified).')
+parser.add_argument('clusterLength',
+                    help='The number of genes in the complete cluster.')
+
+args = parser.parse_args()
+inputDir = args.inputDir
+outputFile = args.outputFile
+clusterLength = int(args.clusterLength)
