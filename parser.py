1e66b9a71763ad6f0292e5771db736959eae362b
CP <christopher.pyatt@postgrad.manchester.ac.uk>
2019-11-21 11:14:45 +0000

    plot code added

--- parser.py
+++ parser.py
@@ -2,8 +2,8 @@ import argparse
 import glob
 import requests
 import csv
-import matplotlib
-import pandas
+import matplotlib.pyplot as plt
+import pandas as pd
 
 parser = argparse.ArgumentParser(description='Input a data file and see some things.')
 parser.add_argument('inFile',
@@ -16,9 +16,11 @@ inFile = args.inFile
 #outFile = args.outFile
 
 #testData = requests.get("https://openprescribing.net/api/1.0/org_code/?q=Beaumont&format=json")
+#https://openprescribing.net/api/1.0/org_details/?org_type=practice&org=14L
 
 try:
 	resp = requests.get(inFile)
+	
 	#print(resp.json())
 	print("Good")
 except:
@@ -26,26 +28,17 @@ except:
 
 
 
-data = pandas.read_csv("test.csv") 
+df = pd.DataFrame(resp.json())
 
-data.head()
+#print(df)
+#print(list(df.columns.values))
 
-'''
-with open("test.csv") as inf:
-	df = csv.reader(inf, delimiter=',')
-	line_count = 0
-	for row in df:
-		if line_count == 0:
-			columnNames = row
-			print(columnNames)
-			#print('Column names are {", ".join(row)}')
-			line_count += 1
-		else:
-			#print(row)
-			line_count += 1
-	print('Processed ' + str(line_count) + ' lines.')
-'''
+#df.plot(x='date', y='female_0_4')
 
+df.set_index('date', inplace=True)
+df.groupby('row_name')['female_0_4'].plot(legend=True)
+
+plt.show()
 
 
 
