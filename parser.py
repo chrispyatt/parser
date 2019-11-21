b5a7289659964194889302ec797920a16b26bca0
CP <christopher.pyatt@postgrad.manchester.ac.uk>
2019-11-21 16:49:56 +0000

    Added date and practice subsetting options

--- parser.py
+++ parser.py
@@ -1,22 +1,47 @@
 import argparse
-import glob
 import requests
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
+from datetime import datetime
+from dateutil.parser import parse
 from pandas.io.json import json_normalize
 
-
+# Specify command line arguments (including defaults for optional args)
 parser = argparse.ArgumentParser(description='Input a data file and see some things.')
 parser.add_argument('inFile',
                     help='Input data file URL (API)')
-#parser.add_argument('outFile',
-#                    help='Output file')
+parser.add_argument('x_axis',
+                    help='What column would you like to use (from your API data) as the x-axis?')
+parser.add_argument('y_axis',
+                    help='What column would you like to use (from your API data) as the y-axis?')
+parser.add_argument('groupby',
+                    help='What column would you like to use (from your API data) to group the data (e.g. by practice)?')
+parser.add_argument('--dateRange', dest='dateRange', default="all",
+                    help='The range of dates (if x axis is time) for which to plot. Format as [date1|date2].')
+parser.add_argument('--groupSubset', dest='groupSubset', default="all",
+                    help='The subset of values (from the groupby) to actually plot. Format as [value1|value2|value3] etc.')
+parser.add_argument('--test', dest='test', default='False',
+                    help='Run the test dataset to make sure things are working.')
 
+# Set given arguments to variables to be used later.
 args = parser.parse_args()
 inFile = args.inFile
-#outFile = args.outFile
+xarg = args.x_axis
+yarg = args.y_axis
+group = args.groupby
+dateRange = args.dateRange
+groupSubset = args.groupSubset
+test = args.test
+
+if test == 'True':
+	inFile = 'https://openprescribing.net/api/1.0/org_details/?org_type=practice&org=14L&format=json'
+	xarg = 'date'
+	yarg = 'star_pu.oral_antibacterials_item'
+	group = 'row_name'
+	dateRange = '2017-04-01|2018-04-01'
+	groupSubset = 'all'
 
 #testData = requests.get("https://openprescribing.net/api/1.0/org_code/?q=Beaumont&format=json")
 #https://openprescribing.net/api/1.0/org_details/?org_type=practice&org=14L&format=json
@@ -30,25 +55,49 @@ except:
 	print("An error occured! Fuck!")
 
 
-#df = pd.DataFrame(resp.json())
-
+# Get nested json into flat dataframe
 df = pd.io.json.json_normalize(resp.json())
 
-#print(list(df.columns.values))
 
-#df2 = df['star_pu'].apply(pd.Series)
-#df3 = pd.concat([df, df2], sort=False, join='inner')
+def plotGraph(dataF):
+	'''
+	This function plots a line graph based on whatever dataframe it is given. The variables plotted are the same as for the rest of the script, but possibly a subset.
+	'''
+	dataF.set_index(xarg, inplace=True)
+	dataF.groupby(group)[yarg].plot(legend=True)
+	plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
 
-#print(df3)
-#print(list(df3.columns.values))
-#print(df3['date'])
+# If both the below options are used, this will subset the dataframe by both.
+if groupSubset != "all" and dateRange != "all":
+	dr = dateRange.split('|')
+	start = datetime.strptime(dr[0], '%Y-%m-%d')
+	end = datetime.strptime(dr[1], '%Y-%m-%d')
+	gS = groupSubset.split('|')
+	df_both = df.loc[(pd.to_datetime(df['date']) >= start) & (pd.to_datetime(df['date']) <= end) & df[group].isin(gS)]
+	plotGraph(df_both)
 
-#df.plot(x='date', y='female_0_4')
-
-df.set_index('date', inplace=True)
-df.groupby('row_name')['star_pu.oral_antibacterials_item'].plot(legend=True)
+# If a date range is specified, this will create a subset of the main dataframe, within the date range.
+elif dateRange != "all":
+	dr = dateRange.split('|')
+	start = datetime.strptime(dr[0], '%Y-%m-%d')
+	end = datetime.strptime(dr[1], '%Y-%m-%d')
+	df_dateRange = df.loc[(pd.to_datetime(df['date']) >= start) & (pd.to_datetime(df['date']) <= end)]
+	plotGraph(df_dateRange)
+	
+# If a subset of the grouping option is specified, this will create a subset of the dataframe, constrained to that subset.
+elif groupSubset != "all":
+	gS = groupSubset.split('|')
+	df_groupSubset = df.loc[df[group].isin(gS)]
+	plotGraph(df_groupSubset)
+	
+else:
+	# Plot xarg against yarg, grouped by group (no subset).
+	plotGraph(df)
 
 plt.show()
+
+
+
 
 
 
