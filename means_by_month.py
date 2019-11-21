583914ed1b171f051f50c97669e8db70542bf872
Simon Lam <simonlam6174@gmail.com>
2019-11-21 16:21:39 +0000

    Actually made it plot avg antibacterial prescribing over entire CCG per month

--- means_by_month.py
+++ means_by_month.py
@@ -1,4 +1,3 @@
 df.set_index('date')
-df.groupby('date').mean().plot()
-df.plot(x='date', y='star_pu.oral_antibacterials_item')
+df.groupby('date')['star_pu.oral_antibacterials.item'].mean().plot()
 plt.legend(loc='center left', bbox_to_anchor=(1.0,0.5))
