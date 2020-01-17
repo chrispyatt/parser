dcb75dd5a9a2d60a1bd080d1d42998546c9b42d3
chrispyatt <34276603+chrispyatt@users.noreply.github.com>
2020-01-17 11:23:11 +0000

    dependencies file added

---
+++ dependencies.sh
@@ -0,0 +1,14 @@
+# Run this file with 'bash dependencies.sh' prior to running the main script, if desired.
+
+pip install argparse
+pip install requests
+pip install csv
+pip install pandas
+pip install json
+pip install datetime
+pip install dateutil
+pip install matplotlib
+pip install scipy
+pip install numpy
+pip install seaborn
+pip install bokeh
